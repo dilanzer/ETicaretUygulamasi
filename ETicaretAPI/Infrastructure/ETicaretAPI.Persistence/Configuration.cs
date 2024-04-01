3d51837c30a4ebd1e2d88977caab8b83e23ae1b9
dilanzer <dilanozermail@gmail.com>
2024-04-01 18:08:11 +0300

    veritabani islemleri

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Configuration.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Configuration;
+
+namespace ETicaretAPI.Persistence;
+
+public static class Configuration
+{
+    public static string ConnectionString
+    {
+        get
+        {
+            ConfigurationManager configurationManager = new();
+            configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ETicaretAPI.API"));
+            configurationManager.AddJsonFile("appsettings.json");
+
+            return configurationManager.GetConnectionString("PostgreSQL");
+        }
+    }
+}
